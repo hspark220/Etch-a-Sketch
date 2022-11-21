a3355f066f413d71620e0b7197c684fd9254bfa5
Heon Soo Park <hspark220@gmail.com>
2022-11-21 12:11:16 -0500

    Make button that will change number of squares
    number of squares per side will be changed via prompt.
    made button bigger to make it more visible.
    button lable "change number of sides"

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@
 // by: Heon Soo Park
 
 let conDim = 500;
-let numSquPerSide = 10;
+let numSquPerSide = 5;
 let sqrWidth = conDim/numSquPerSide;
 
 const container = document.querySelector('.main-container');
@@ -14,11 +14,31 @@ pseudo code for looping the squares
  - for loop inside while going through each rows
 
 */
+const updateScreen = () => {
+    //remove old squares
+    const oldSquares = document.querySelectorAll('.squares');
+    oldSquares.forEach(square => {
+        square.remove();
+    });
 
-for (let i = 0; i < numSquPerSide**2; i++) {
-    const squares = document.createElement('div');
-    squares.classList.add('squares');
-    squares.style.cssText = `width: ${sqrWidth}px; height: ${sqrWidth}px;`
-    container.appendChild(squares);
-}
+    sqrWidth = conDim/numSquPerSide;
+    for (let i = 0; i < numSquPerSide**2; i++) {
+        const squares = document.createElement('div');
+        squares.classList.add('squares');
+        squares.style.cssText = `width: ${sqrWidth}px; height: ${sqrWidth}px;`
+        container.appendChild(squares);
+    }
     
+}
+
+updateScreen();
+
+//button for changing the number of squares per side
+const changeNumberOfSquare = () => {
+    const newNumSquPerSides = prompt("Choose the number of sides for the square");
+    numSquPerSide = newNumSquPerSides < 100 ? newNumSquPerSides : 100;
+    updateScreen();
+}
+
+const changeNumSideBtn = document.querySelector('#change-number-of-sides');
+changeNumSideBtn.addEventListener('click', changeNumberOfSquare);
