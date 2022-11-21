e0dd69013d7078f2a2c63bde799c1da15701ccfd
Heon Soo Park <hspark220@gmail.com>
2022-11-21 11:41:46 -0500

    Make looping logic for number of square by side
    a loop that goes through and makes squares inside the container
    (number of sides)^2 times.

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,24 @@
+// Etch a Sketch project by Odin Project
+// by: Heon Soo Park
 
+let conDim = 500;
+let numSquPerSide = 10;
+let sqrWidth = conDim/numSquPerSide;
 
 const container = document.querySelector('.main-container');
-container.style.cssText = `width: ${500}px; height: ${500}px;`;
+container.style.cssText = `width: ${conDim}px; height: ${conDim}px;`;
 
-for (let i = 0; i < 25; i++) {
+/*
+pseudo code for looping the squares
+ - while loop checking if the height of square has been passed
+ - for loop inside while going through each rows
+
+*/
+
+for (let i = 0; i < numSquPerSide**2; i++) {
     const squares = document.createElement('div');
     squares.classList.add('squares');
-    squares.style.cssText = `width: ${500/5}px; height: ${500/5}px;`
+    squares.style.cssText = `width: ${sqrWidth}px; height: ${sqrWidth}px;`
     container.appendChild(squares);
 }
     
